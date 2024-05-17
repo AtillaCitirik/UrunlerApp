a709a2955d9dbe3f7ff47146cb417e0f27411d4b
atilla.citirik <atillactrk@gmail.com>
2024-05-17 12:58:44 +0300

    Initial Commit

---
+++ UrunlerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UrunlerApp
+//
+//  Created by Atilla Çıtırık on 17.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
